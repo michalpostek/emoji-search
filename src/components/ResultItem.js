8597664fab491a894d7bb112c638d32188fcdbef
Michał Postek <postek.michal@protonmail.com>
2022-04-07 14:35:05 +0200

    copying to clipboard by click on emoji

--- src/components/ResultItem.js
+++ src/components/ResultItem.js
@@ -1,13 +1,17 @@
 import PropTypes from "prop-types";
 
+import { copyToClipboard } from "../helpers/copyToClipboard";
+
 import { StyledResultItem } from "./styles/Results.styled";
 
 const ResultItem = ({ emoji: { title, symbol } }) => {
     const codePointHex = symbol.codePointAt(0).toString(16);
     const src = `//cdn.jsdelivr.net/emojione/assets/png/${codePointHex}.png`;
 
+    const handleCopyToClipboard = () => copyToClipboard(symbol);
+
     return (
-        <StyledResultItem>
+        <StyledResultItem onClick={handleCopyToClipboard}>
             <img src={src} alt={title} />
             <p>{title}</p>
         </StyledResultItem>
