5024105e7728eed6f2c2d2b32c55eed708c74058
Michał Postek <postek.michal@protonmail.com>
2022-04-07 12:36:47 +0200

    result styles

--- src/components/styles/Results.styled.js
+++ src/components/styles/Results.styled.js
@@ -1,13 +1,38 @@
 import styled from "styled-components";
 
 export const StyledResults = styled.div`
-    
+    margin: 5vh 7vw;
+    display: flex;
+    justify-content: center;
 `;
 
 export const StyledResultsList = styled.div`
-
+    display: grid;
+    grid-template-columns: repeat(12, 1fr);
+    grid-gap: 1vh;
+    grid-auto-rows: 1fr;
 `;
 
 export const StyledResultItem = styled.div`
-    
+    border-radius: 1vw;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 0.5vh;
+    transition: 0.1s ease-in;
+
+    &:hover {
+        cursor: pointer;
+        background-color: rgb(220, 220, 220);
+    }
+
+    img {
+        height: 7vh;
+        margin-bottom: 1vh;
+    }
+
+    p {
+        text-align: center;
+    }
 `;
--- src/helpers/filterEmojis.js
+++ src/helpers/filterEmojis.js
@@ -3,5 +3,5 @@ import emojiList from "../data/emojiList.json";
 export const filterEmojis = keyword => {
     return emojiList.filter(
         emoji => emoji.title.includes(keyword) || emoji.keywords.includes(keyword)
-    ).splice(0, 100);
+    ).splice(0, 144);
 };
